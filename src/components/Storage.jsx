3c365c754dcd46a13fcc9ed80b6887ba0c537ca2
kentaurse <kentaurse0212@gmail.com>
2024-08-15 15:19:44 +0900

    feat: complete UI

---
+++ src/components/Storage.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Group from './Group';
+import { Form, Tabs, Select, Input, DatePicker, TimePicker, Radio, Space } from 'antd';
+
+const { TextArea } = Input;
+
+const SubcontractPayment = ({ className = '' }) => {
+  const tabNames = ['配車1', '配車2', '配車3']
+  return (
+    <div className={`${className}`}>
+      <Group label={"保管"}>
+        <Form.Item label={"保管場所"}>
+          <Select defaultValue={1} />
+        </Form.Item>
+        <div className='flex flex-wrap flex-row items-center gap-x-4'>
+          <Form.Item label={"入庫日"} style={{ width: 100 }} className='grow'>
+            <DatePicker className='w-full' />
+          </Form.Item>
+          <Form.Item label={"出庫日"} style={{ width: 100 }} className='grow'>
+            <DatePicker className='w-full' />
+          </Form.Item>
+        </div>
+        <Group label={"料金"}>
+          <div className='flex flex-wrap flex-row items-center gap-x-4'>
+            <Form.Item label={"荷主リフトオフ"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Form.Item label={"荷主リフトオン"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Form.Item label={"荷主保管/日"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Radio.Group value="{1}">
+              <div className='flex flex-col'>
+                <Radio value="{1}">課税</Radio>
+                <Radio value="{2}">非課税</Radio>
+              </div>
+            </Radio.Group>
+          </div>
+          <div className='flex flex-wrap flex-row items-center gap-x-4'>
+            <Form.Item label={"下払リフトオフ"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Form.Item label={"下払リフトオン"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Form.Item label={"下払保管/日"} className='grow w-32'>
+              <Input required className='w-full' />
+            </Form.Item>
+            <Radio.Group value="{1}">
+              <div className='flex flex-col'>
+                <Radio value="{1}">課税</Radio>
+                <Radio value="{2}">非課税</Radio>
+              </div>
+            </Radio.Group>
+          </div>
+        </Group>
+        <Group label={"備考"}>
+          <Form.Item label={"リクエストフォーム備考欄"} rules={[{ required: true }]}>
+            <div className='flex flex-wrap flex-row items-center gap-x-4'>
+              <TextArea rows={4} className='grow' />
+            </div>
+          </Form.Item>
+          <Form.Item label={"送り状・受領書備考欄"} rules={[{ required: true }]}>
+            <div className='flex flex-wrap flex-row items-center gap-x-4'>
+              <TextArea rows={4} className='grow' />
+            </div>
+          </Form.Item>
+        </Group>
+      </Group>
+    </div>
+  );
+};
+
+export default SubcontractPayment;
---
+++ src/components/SubcontractPayment.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import Group from './Group';
+import { Form, Tabs, Select, Input, DatePicker, TimePicker, Radio, Space } from 'antd';
+
+const TabForm = () => {
+  return (
+    <div>
+      <Form.Item label={"会社名"}>
+        <Select defaultValue={1} />
+      </Form.Item>
+      <div className='flex flex-wrap flex-row items-center gap-x-4'>
+        <Form.Item label={"料金"} style={{ width: 100 }} className='grow'>
+          <Select defaultValue={1} />
+        </Form.Item>
+        <Radio.Group value="{1}">
+          <div className='flex flex-col'>
+            <Radio value="{1}">課税</Radio>
+            <Radio value="{2}">非課税</Radio>
+          </div>
+        </Radio.Group>
+        <Radio.Group value="{1}">
+          <div className='flex flex-col'>
+            <Radio value="{1}">自車</Radio>
+            <Radio value="{2}">平凡な車</Radio>
+          </div>
+        </Radio.Group>
+      </div>
+      <div className='flex flex-wrap flex-row items-center gap-x-4'>
+        <Form.Item label={"車番1"} className='grow'>
+          <Select defaultValue={1} />
+        </Form.Item>
+        <Form.Item label={"車番2"} className='grow'>
+          <Select defaultValue={1} />
+        </Form.Item>
+        <Form.Item label={"車番3"} className='grow'>
+          <Select defaultValue={1} />
+        </Form.Item>
+      </div>
+      <Form.Item label={"高速道路料金"} className=' grow'>
+        <Input />
+      </Form.Item>
+      <div className='flex flex-wrap flex-row items-center gap-x-4'>
+        <Form.Item label={"スケール費"} className='grow w-32'>
+          <Input required className='w-full' />
+        </Form.Item>
+        <Radio.Group value="{1}">
+          <div className='flex flex-col'>
+            <Radio value="{1}">課税</Radio>
+            <Radio value="{2}">非課税</Radio>
+          </div>
+        </Radio.Group>
+        <Form.Item label={"シャーシ留置費"} className='grow w-32'>
+          <Input required className='w-full' />
+        </Form.Item>
+        <Radio.Group value="{1}">
+          <div className='flex flex-col'>
+            <Radio value="{1}">課税</Radio>
+            <Radio value="{2}">非課税</Radio>
+          </div>
+        </Radio.Group>
+      </div>
+      <div className='flex flex-wrap flex-row items-center gap-x-4'>
+        <Form.Item label={"その他費用"} className='grow w-32'>
+          <Input required className='w-full' />
+        </Form.Item>
+        <Radio.Group value="{1}">
+          <div className='flex flex-col'>
+            <Radio value="{1}">課税</Radio>
+            <Radio value="{2}">非課税</Radio>
+          </div>
+        </Radio.Group>
+      </div>
+    </div>
+  )
+}
+const SubcontractPayment = ({ className = '' }) => {
+  const tabNames = ['配車1', '配車2', '配車3']
+  return (
+    <div className={`${className}`}>
+      <Group label={"下払"}>
+        <Tabs type='card' className='w-full' items={tabNames.map((item, index) => {
+          return {
+            label: item,
+            key: index,
+            children: <TabForm />
+          }
+        })} />
+      </Group>
+    </div>
+  );
+};
+
+export default SubcontractPayment;
